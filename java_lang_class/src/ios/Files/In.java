3958f0fc46c12fbd0bcfa63c49e1a0347126af0e
juninhocb <juninhocb@hotmail.com>
2023-05-04 15:26:42 -0300

    More about files

--- java_lang_class/src/ios/Files/In.java
+++ java_lang_class/src/ios/Files/In.java
@@ -1,22 +1,67 @@
 package ios.Files;
 
 
+import ios.Information;
+
+import javax.sound.sampled.Line;
 import javax.swing.*;
 import java.io.*;
-import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 public class In {
 
     private static final String PATH = String.format("%s/files/text.txt", System.getProperty("user.dir"));
-    public void runMe() throws IOException {
+    public void runMe() throws IOException, ClassNotFoundException {
 
         //ex1(); //bufferedReader requires a reader [FileReader, InputStreamReader] are examples.
         //ex2();
+        //ex3();
+        //ex4();
+        ex5();
 
+
+    }
+
+    public void ex5() throws IOException, ClassNotFoundException {
+        File file = new File(String.format("%s/files/info.txt", System.getProperty("user.dir")));
+        FileInputStream fis = new FileInputStream(file);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Information info = (Information) ois.readObject();
+        System.out.println(info.toString());
+    }
+
+    public void ex4(){
+
+        File fileForTest = new File(System.getProperty("user.dir") + "/files/log/test");
+        if (!fileForTest.exists())
+            fileForTest.mkdirs(); //create both: log and test
+
+        File file = new File(System.getProperty("user.dir") + "/files/log");
+        if (!file.exists())
+            file.mkdir(); //create only log. Does not have effect within two directories path
+
+        System.out.println("Date and Time " + new SimpleDateFormat("dd-MM-yyyy hh-mm-ss").format(new Date()));
+        System.out.println("Full Date " + new Date());
+        //changing the timezone
+        ZoneId zoneId = ZoneId.of("GMT+9"); // time in Japan
+        TimeZone.setDefault(TimeZone.getTimeZone(zoneId));
+        System.out.println("Full date Japan " + new Date());
+        System.out.println("Get separator " + System.getProperty("file.separator"));
+
+        File newFile = new File(System.getProperty("user.dir") + "/files");
+        List<File> files = Arrays.asList(newFile.listFiles());
+        files.forEach(x -> System.out.println(x.getName()));
+    }
+
+    public void ex3(){
         JFileChooser chooser = new JFileChooser();
         int returnVal = chooser.showOpenDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -38,7 +83,6 @@ public class In {
         } else {
             JOptionPane.showMessageDialog(null, "You need to chose a File.");
         }
-
     }
 
     public void ex2() throws IOException {
---
+++ java_lang_class/src/ios/Information.java
@@ -0,0 +1,65 @@
+package ios;
+
+import jdk.internal.org.objectweb.asm.commons.SerialVersionUIDAdder;
+
+import java.io.Serializable;
+
+public class Information implements Serializable {
+    private static final long serialVersionUID = 1L; //can get properly using serialver
+    private transient String internalCode;
+    private String serverName;
+    private String serverIp;
+    private int portNo;
+    private boolean isAllow;
+
+    public Information(String ic, String sn, String sip, int pNo, boolean allow){
+        this.internalCode = ic;
+        this.serverName = sn;
+        this.serverIp = sip;
+        this.portNo = pNo;
+        this.isAllow = allow;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getServerIp() {
+        return serverIp;
+    }
+
+    public void setServerIp(String serverIp) {
+        this.serverIp = serverIp;
+    }
+
+    public int getPortNo() {
+        return portNo;
+    }
+
+    public void setPortNo(int portNo) {
+        this.portNo = portNo;
+    }
+
+    public boolean isAllow() {
+        return isAllow;
+    }
+
+    public void setAllow(boolean allow) {
+        isAllow = allow;
+    }
+
+    @Override
+    public String toString() {
+        return "Information{" +
+                "internalCode='" + internalCode + '\'' +
+                ", serverName='" + serverName + '\'' +
+                ", serverIp='" + serverIp + '\'' +
+                ", portNo=" + portNo +
+                ", isAllow=" + isAllow +
+                '}';
+    }
+}
